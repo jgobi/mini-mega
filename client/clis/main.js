aff8ed657ee659b5fbdd7e08348559240a0b3f04
João Gabriel <joaogabrieloliveira9@gmail.com>
2020-12-13 13:19:00 -0300

    history

--- client/clis/main.js
+++ client/clis/main.js
@@ -1,5 +1,7 @@
 const vorpal = new (require('vorpal'))();
 
+vorpal.history('mini-mega-client-main');
+
 vorpal.use(require('../commands/register'));
 vorpal.use(require('../commands/login'));
 
--- client/clis/user.js
+++ client/clis/user.js
@@ -1,5 +1,7 @@
 const vorpal = new (require('vorpal'))();
 
+vorpal.history('mini-mega-client-user');
+
 vorpal.find('exit').remove();
 vorpal.use(require('../commands/logout'));
 vorpal.find('logout').alias('exit');
