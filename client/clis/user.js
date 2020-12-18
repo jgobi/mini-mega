dcf1e22947aec92501718c9cfa0a822bf5dc6025
João Gabriel <joaogabrieloliveira9@gmail.com>
2020-12-18 16:39:01 -0300

    encrypt command

--- client/clis/user.js
+++ client/clis/user.js
@@ -5,5 +5,6 @@ vorpal.history('mini-mega-client-user');
 vorpal.use(require('../commands/logout'));
 vorpal.use(require('../commands/system'));
 vorpal.use(require('../commands/remote'));
+vorpal.use(require('../commands/encrypt'));
 
 module.exports = vorpal;
---
+++ client/commands/encrypt.js
@@ -0,0 +1,91 @@
+const store = require('../store');
+const fs = require('fs');
+const path = require('path');
+const { createCipheriv } = require('crypto');
+
+const autocomplete = (input) => {
+    try {
+        let dest = path.isAbsolute(input) ? input : store.localFolder + '/' + input;
+        dest = dest.endsWith('/') ? dest : path.dirname(dest);
+        let files = fs.readdirSync(dest, { withFileTypes: true }).map(a => a.isDirectory() ? a.name + '/' : a.name);
+        files.unshift('../');
+        return files;
+    } catch (err) {
+        return [];
+    }
+}
+
+
+/**
+ * @param {string} fullPath 
+ * @param {string} destination 
+ */
+function encrypt (fullPath, destination) {
+    const CHUNK_SIZE = 0x1000000; // pelo menos 0x100000
+    const fileName = Buffer.from(path.basename(fullPath), 'utf-8');
+    if (fileName.length > 255) return console.error('Filename too long');
+
+    const { generateFileKey, obfuscateFileKey } = require('../helpers/keys');
+    const { encryptChunk, encryptInfo } = require('../helpers/encrypt');
+    const { encodeInfoFileV1 } = require('../helpers/infoFile');
+    const { readFileChunk } = require('../helpers/readFile');
+
+    let { key, nonce } = generateFileKey();
+
+    let paddingSize = 0;
+    let condensedMac = Buffer.alloc(16);
+    let macs = [];
+    let fileSize = 0;
+
+    const fileGenerator = readFileChunk(fullPath, CHUNK_SIZE);
+
+    fs.writeFileSync(destination, Buffer.alloc(0));
+    let ctr = 0;
+    let i = 0;
+    for (let buf of fileGenerator) {
+        if (!buf) break;
+        fileSize += buf.length;
+
+        let enc = encryptChunk(buf, key, nonce, ctr, condensedMac);
+        ctr = enc.ctr;
+        paddingSize = enc.paddingSize;
+        condensedMac = enc.condensedMac;
+        macs.push(...enc.macs);
+        
+        fs.appendFileSync(destination, enc.encryptedChunk);
+    }
+
+    let encryptedInfoFile = encryptInfo(encodeInfoFileV1(fileSize, fileName, macs), key);
+    fs.writeFileSync(destination + '.info', encryptedInfoFile);
+
+    let obfuscatedFileKey = obfuscateFileKey(key, nonce, condensedMac);
+
+    const cipher = createCipheriv('aes-128-ecb', store.masterKey, '').setAutoPadding(false);
+    const encryptedMasterKey = Buffer.concat([cipher.update(obfuscatedFileKey), cipher.final()]);
+
+    fs.writeFileSync(destination + '.key', encryptedMasterKey);
+}
+
+/**
+ * @param {import('@types/vorpal')} vorpal 
+ * @param {any} options 
+ */
+module.exports = function (vorpal, options) {
+    vorpal
+    .command('encrypt <input> [output]', 'Encrypt file using the master key')
+    .autocomplete({
+        data: autocomplete,
+    })
+    .action(async function (args) {
+        const input = path.isAbsolute(args.input) ? args.input : path.join(store.localFolder, args.input);
+        const outputArg = args.output || input + '.enc';
+        const output = path.isAbsolute(outputArg) ? outputArg : path.join(store.localFolder, outputArg);
+        this.log('Encrypting file "' + input + '" and saving result as "' + output + '".')
+        try {
+            encrypt(input, output);
+            this.log('Done');
+        } catch (err) {
+            this.log('Error while encrypting:' + err.toString());
+        }
+    });
+};
